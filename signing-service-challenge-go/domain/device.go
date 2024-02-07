a81a34f9a102b7bcddcc4f5c8f8741c09c50d0bb
rossola <rossola@tecniplast.it>
2024-02-07 22:56:02 +0100

    implemented base API functions

--- signing-service-challenge-go/domain/device.go
+++ signing-service-challenge-go/domain/device.go
@@ -1,3 +1,67 @@
 package domain
 
-// TODO: signature device domain model ...
+import "sync"
+
+// Algorithm represents the supported algorithms.
+type Algorithm int
+
+// Constants representing supported algorithms.
+const (
+	ECC Algorithm = iota + 1
+	RSA
+)
+
+//// Validate checks if the given algorithm is supported.
+//func (a Algorithm) Validate() (crypto.ECCKeyPair, crypto.RSAKeyPair, error) {
+//	switch a {
+//	case ECC:
+//		return crypto.ECCKeyPair{}, nil, nil
+//	case RSA:
+//		return crypto.RSAKeyPair, nil
+//	default:
+//		return nil, errors.New("unsupported algorithm")
+//	}
+//}
+//
+//func (a Algorithm) ValidateRSA() (crypto.RSAGenerator, error) {
+//	switch a {
+//	case RSA:
+//		return crypto.RSAKeyPair{}, nil
+//	default:
+//		return nil, errors.New("unsupported algorithm")
+//	}
+//}
+
+type SignatureDevice struct {
+	ID               string    `json:"id"`
+	Algorithm        Algorithm `json:"algorithm"`
+	Label            *string   `json:"label"`
+	SignatureCounter *int      `json:"signatureCounter"`
+}
+
+type CreateSignatureDeviceResponse struct {
+	// Define your response fields here
+}
+
+type SignatureService struct {
+	Devices map[string]*SignatureDevice
+	Mutex   sync.Mutex
+}
+
+func NewSignatureService() *SignatureService {
+	return &SignatureService{
+		Devices: make(map[string]*SignatureDevice),
+	}
+}
+
+// CreateSignatureDeviceRequest represents the request body for creating a signature device.
+type CreateSignatureDeviceRequest struct {
+	Algorithm Algorithm `json:"algorithm"`
+	Label     *string   `json:"label,omitempty"`
+}
+
+// SignatureResponse represents the response body for signing a transaction.
+type SignatureResponse struct {
+	Signature  string `json:"signature"`
+	SignedData string `json:"signed_data"`
+}
--- signing-service-challenge-go/main.go
+++ signing-service-challenge-go/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"log"
-
+	"encoding/base64"
 	"github.com/fiskaly/coding-challenges/signing-service-challenge/api"
+	device "github.com/fiskaly/coding-challenges/signing-service-challenge/domain"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
 )
 
 const (
@@ -12,9 +18,88 @@ const (
 )
 
 func main() {
+	l, _ := zap.NewDevelopment()
+	logger := l.Sugar()
+	zap.RedirectStdLog(logger.Desugar())
+	zap.ReplaceGlobals(logger.Desugar())
+	defer func(logger *zap.SugaredLogger) {
+		err := logger.Sync()
+		if err != nil {
+
+		}
+	}(logger)
+	logger.Info("Starting server on " + ListenAddress)
 	server := api.NewServer(ListenAddress)
 
+	r := gin.Default()
+
+	signatureService := device.NewSignatureService()
+
+	r.POST("/signature-device", func(c *gin.Context) {
+		var req device.CreateSignatureDeviceRequest
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Generate a unique ID for the device (for simplicity, we use a timestamp)
+		id := base64.StdEncoding.EncodeToString([]byte(time.Now().String()))
+
+		// Create a new signature device
+		signatureDevice := &device.SignatureDevice{
+			ID:               id,
+			Algorithm:        req.Algorithm,
+			Label:            req.Label,
+			SignatureCounter: new(int),
+		}
+
+		// Store the signature device
+		signatureService.Mutex.Lock()
+		defer signatureService.Mutex.Unlock()
+		signatureService.Devices[id] = signatureDevice
+
+		// Respond with success
+		c.JSON(http.StatusCreated, gin.H{"id": id})
+	})
+
+	// Endpoint to sign a transaction
+	r.POST("/sign-transaction", func(c *gin.Context) {
+		// Retrieve device ID and data from the request
+		deviceID := c.PostForm("deviceId")
+		data := c.PostForm("data")
+
+		// Find the signature device
+		signatureService.Mutex.Lock()
+		thisDevice, found := signatureService.Devices[deviceID]
+		signatureService.Mutex.Unlock()
+		if !found {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Signature device not found"})
+			return
+		}
+
+		// Construct secured data to be signed
+		securedDataToBeSigned := "_" + data + "_" + base64.StdEncoding.EncodeToString([]byte(thisDevice.ID))
+
+		logger.Info("Secured data to be signed: " + securedDataToBeSigned)
+		// Increment signature counter
+		*thisDevice.SignatureCounter++
+
+		// Generate signature (for demonstration, we use a placeholder)
+		signature := "base64_encoded_signature"
+
+		// Construct signed data
+		signedData := strconv.Itoa(*thisDevice.SignatureCounter) + "_" + data + "_" + base64.StdEncoding.EncodeToString([]byte(thisDevice.ID))
+
+		// Respond with signature response
+		c.JSON(http.StatusOK, device.SignatureResponse{
+			Signature:  signature,
+			SignedData: signedData,
+		})
+	})
+
+	// Run the server
 	if err := server.Run(); err != nil {
 		log.Fatal("Could not start server on ", ListenAddress)
 	}
+
 }
